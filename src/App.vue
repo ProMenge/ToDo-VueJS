244e0cc8c7c5b455a7fc3a01eff6080d721943b5
Fred Menge <mengeprofissional@gmail.com>
2024-07-10 14:06:55 -0300

    :bug: fix: Erro na funcionalidade de filtros

--- src/App.vue
+++ src/App.vue
@@ -4,54 +4,55 @@ import TaskHeader from './components/TaskHeader.vue';
 import Form from './components/Form.vue';
 import TaskList from './components/TaskList.vue';
 
-
 const state = reactive({
   filter: "All Tasks",
   taskTemp: '',
-  tasks: [
+  tasks: []
+});
 
-  ]
-})
-
-const changeFilter = (event) => { state.filter = event.target.value }
+const changeFilter = (event) => { state.filter = event.target.value };
 
 const getTaskToDo = () => {
-  return state.tasks.filter(task => !task.finished)
-}
+  return state.tasks.filter(task => !task.finished);
+};
 
 const getTaskFinished = () => {
-  return state.tasks.filter(task => task.finished)
-}
+  return state.tasks.filter(task => task.finished);
+};
 
-const getFilteredTasks = () => {
+// Computed property to return sorted tasks
+const finishedTaskToFinal = computed(() => {
+  return state.tasks.slice().sort((a, b) => a.finished - b.finished);
+});
+
+// Updated getFilteredTasks to use finishedTaskToFinal
+const getFilteredTasks = computed(() => {
   switch (state.filter) {
     case 'toDo':
-      return getTaskToDo();
+      return finishedTaskToFinal.value.filter(task => !task.finished);
     case 'finished':
-      return getTaskFinished();
+      return finishedTaskToFinal.value.filter(task => task.finished);
     default:
-      return state.tasks;
+      return finishedTaskToFinal.value;
   }
-}
+});
 
 const registerTask = () => {
   const newTask = {
     title: state.taskTemp,
     finished: false,
-  }
-  state.tasks.push(newTask)
-  state.taskTemp = ''
-
-}
+  };
+  state.tasks.push(newTask);
+  state.taskTemp = '';
+};
 
 const editTaskTemp = (event) => {
-
-  state.taskTemp = event.target.value
-}
+  state.taskTemp = event.target.value;
+};
 
 const allTasksFinished = () => {
   return state.filter === 'toDo' && getTaskToDo().length === 0 && state.tasks.length >= 1;
-}
+};
 
 const isTaskListEmpty = () => {
   return state.tasks.length === 0;
@@ -62,24 +63,15 @@ const removeTask = (task) => {
   if (index > -1) {
     state.tasks.splice(index, 1);
   }
-}
-
-const finishedTaskToFinal = computed(() => {
-  return state.tasks.slice().sort((a,b) => a.finished - b.finished)
-
-});
-// const verifyCheckedTask = (event) => { task.finished = event.target.checked }
-
+};
 </script>
 
 <template>
-
   <div class="container">
     <TaskHeader :tasks-to-do="getTaskToDo().length" />
     <Form :task-temp="state.taskTemp" :edit-task-temp="editTaskTemp" :register-task="registerTask"
       :change-filter="changeFilter" />
-    <TaskList :get-filtered-tasks="getFilteredTasks()" :all-tasks-finished="allTasksFinished()"
-      :is-task-list-empty="isTaskListEmpty()" :remove-task="removeTask" :finished-task-to-final="finishedTaskToFinal"/>
+    <TaskList :get-filtered-tasks="getFilteredTasks" :all-tasks-finished="allTasksFinished()" :is-task-list-empty="isTaskListEmpty()" :remove-task="removeTask" />
   </div>
 </template>
 
--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,43 +1,32 @@
 <script setup>
-
-const props = defineProps(['getFilteredTasks', 'allTasksFinished', 'isTaskListEmpty', 'removeTask', 'finishedTaskToFinal']);
-
+const props = defineProps(['getFilteredTasks', 'allTasksFinished', 'isTaskListEmpty', 'removeTask']);
 </script>
 
 <template>
-     <span v-if="props.isTaskListEmpty" class=" generalBackgroundColor text-white text-center fw-semibold w-50 justify-content-center  d-flex rounded-3 position-absolute start-50 translate-middle mt-4 ">List is empty! Let's try add a new
-          task to
-          do!</span>
-
-     <ul class=" list-group mt-4">
-          <li class="list-group-item d-flex align-items-center task" v-for="task in props.finishedTaskToFinal" :key="task.title">
-               <input @change="event => task.finished = event.target.checked" :checked="task.finished" :id="task.title"
-                    type="checkbox">
-               <label :class="{ done: task.finished }" class="ms-3  text-white " :for="task.title">
-                    {{ task.title }}
-               </label>
-               <button @click="props.removeTask(task)" type="button"
-                    class="btn btn-outline-danger btn-sm  position-absolute end-0 me-2">X</button>
-          </li>
-     </ul>
-     <span v-if="props.allTasksFinished" class="text-center fw-semibold w-50 justify-content-center text-white d-flex rounded-3 generalBackgroundColor position-absolute start-50 translate-middle mt-4 ">There aren't any tasks to do!
-          Great Job!</span>
+  <span v-if="props.isTaskListEmpty" class=" generalBackgroundColor text-white text-center fw-semibold w-50 justify-content-center d-flex rounded-3 position-absolute start-50 translate-middle mt-4">List is empty! Let's try add a new task to do!</span>
+  <ul class="list-group mt-4">
+    <li class="list-group-item d-flex align-items-center task" v-for="task in props.getFilteredTasks" :key="task.title">
+      <div>
+        <input @change="event => task.finished = event.target.checked" :checked="task.finished" :id="task.title" type="checkbox">
+        <label :class="{ done: task.finished }" class="ms-3 text-white" :for="task.title">
+          {{ task.title }}
+        </label>
+      </div>
+      <button @click="props.removeTask(task)" type="button" class="btn btn-outline-danger btn-sm position-absolute end-0 me-2">X</button>
+    </li>
+  </ul>
+  <span v-if="props.allTasksFinished" class="text-center fw-semibold w-50 justify-content-center text-white d-flex rounded-3 generalBackgroundColor position-absolute start-50 translate-middle mt-4">There aren't any tasks to do! Great Job!</span>
 </template>
 
-<!-- ADICIONAR NÃO HÁ TAREFAS PENDENTES + BOTÃO REMOVER TAREFA  -->
-
 <style scoped>
-     .done {
-          text-decoration: line-through;
-     }
-
-     .generalBackgroundColor{
-          background-color: #1e1e1e;
-     }
-
-     .task{
-
-          background-color: #4e4e4e;
-          border-color: white;
-     }
+.done {
+  text-decoration: line-through;
+}
+.generalBackgroundColor {
+  background-color: #1e1e1e;
+}
+.task {
+  background-color: #4e4e4e;
+  border-color: white;
+}
 </style>
